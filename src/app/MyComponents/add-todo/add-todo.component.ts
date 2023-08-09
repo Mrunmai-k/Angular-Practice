8b46468858a01b1fc24315ac4d7276d852b88fb5
mrunmai <mrunmaikudale18@gmail.com>
2023-08-10 01:42:48 +0530

    created a component for adding the todos

--- src/app/MyComponents/add-todo/add-todo.component.ts
+++ src/app/MyComponents/add-todo/add-todo.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { ToDo } from 'src/app/ToDo';
 
 @Component({
   selector: 'app-add-todo',
   templateUrl: './add-todo.component.html',
   styleUrls: ['./add-todo.component.css']
 })
-export class AddTodoComponent {
+export class AddTodoComponent 
+{
+  title!: string;
+  desc!: string;
+
+  @Output() todoAdd: EventEmitter<ToDo> = new EventEmitter; 
+
+  onSubmit(){
+    const todo = {
+      sno:8,
+      title: this.title,
+      desc: this.desc,
+      active: true
+    }
+    this.todoAdd.emit(todo);
+  }
 
 }
