423fa4ba43c2bd7b844f900572c9edd8a488dbc4
mrunmai <mrunmaikudale18@gmail.com>
2023-08-10 00:15:34 +0530

    created 2 different components for listing the todos and working on the items

---
+++ src/app/MyComponents/todo-item/todo-item.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ToDo } from 'src/app/ToDo';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent {
+  @Input() todo!: ToDo;
+
+  @Output() todoDelete: EventEmitter<ToDo> = new EventEmitter; 
+
+
+  constructor() { }
+
+  onClick(todo: ToDo){
+    this.todoDelete.emit(todo);
+    console.log("onClick method has been triggered...");
+  }
+}
--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToDo } from 'src/app/ToDo';
 
 @Component({
   selector: 'app-todos',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent {
+  todos: ToDo[];
 
+  constructor() {
+    this.todos = [
+      {
+        sno: 1,
+        title: 'This is title1',
+        desc: 'This is Description',
+        active: true
+      },
+      {
+        sno: 2,
+        title: 'This is title2',
+        desc: 'This is Description',
+        active: true
+      },
+      {
+        sno: 3,
+        title: 'This is title3',
+        desc: 'This is Description',
+        active: true
+      }
+    ];
+  }
+
+  deleteTodo(todo:ToDo){
+    console.log(todo);
+    const index = this.todos.indexOf(todo);
+    this.todos.splice(index,1);
+  }
 }
